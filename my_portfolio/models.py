f139d30db21c3bad6c867ec70171f4099aa6048c
Victor <victorokolie001@gmail.com>
2025-10-01 18:32:08 +0100

    fixed seo

--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -12,7 +12,7 @@ User = get_user_model()
 class Service(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
-    icon = models.CharField(max_length=50)
+    icon = models.TextField()
 
     def __str__(self):
         return self.title
