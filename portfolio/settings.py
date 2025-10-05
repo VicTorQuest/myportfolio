94971e31875b5021c1c6c5c672ab0778c8babed7
Victor <victorokolie001@gmail.com>
2025-10-05 13:09:37 +0100

    worked with schema

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -106,20 +106,20 @@ DB_POOLED = os.environ.get("DATABASE_URL_POOLED")
 DB_DIRECT = os.environ.get("NEON_DIRECT_URL") or os.environ.get("DATABASE_URL")
 
 
-# DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL"),
-#         conn_max_age=0,
-#         ssl_require=True
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=0,
+        ssl_require=True
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 LOGGING = {
